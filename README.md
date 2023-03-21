8cd02683f1ff456f224b3a68f1efe909d8e5b247
boosteffect <35601682+boosteffect@users.noreply.github.com>
2023-03-21 15:25:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngx-bootstrap-collapse-r6wdkn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngx-bootstrap-collapse-r6wdkn)
---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  isCollapsed = false;
+  isCollapsed2 = false;
+  isCollapsed3 = false;
+  isCollapsed4 = false;
+}
